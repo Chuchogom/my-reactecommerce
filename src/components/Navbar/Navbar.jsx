192c95f5c80eb0ed901bcff782ac4c66c9583ba6
Jesusgomrec <90487411+Chuchogom@users.noreply.github.com>
2022-08-12 08:31:25 -0500

    Desafio 2 Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import logo from '../../assets/logoShop.svg';
+import menu from '../../assets/menu.svg';
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <nav>
+            <img src={menu} alt="menu" className="menu" />
+            <div className="navbar-left">
+                <img src={logo} alt="logo" className="nav-logo" />
+                <ul>
+                    <li>
+                        <a href="/">All</a>
+                    </li>
+                    <li>
+                        <a href="/">Clothes</a>
+                    </li>
+                    <li>
+                        <a href="/">Electronics</a>
+                    </li>
+                    <li>
+                        <a href="/">Furnitures</a>
+                    </li>
+                    <li>
+                        <a href="/">Toys</a>
+                    </li>
+                    <li>
+                        <a href="/">Others</a>
+                    </li>
+                </ul>
+            </div>
+            <div className="navbar-right">
+                <ul>
+                    <li className="navbar-email">
+                        email@example.com
+                    </li>
+                    <li className="navbar-shopping-cart">
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
