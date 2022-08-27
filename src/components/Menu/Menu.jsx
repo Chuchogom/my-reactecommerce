a0bd2e07a9d9445629e325476df3714ce9e216c6
Jesusgomrec <90487411+Chuchogom@users.noreply.github.com>
2022-08-27 12:53:53 -0500

    Update Menu.jsx

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import './Menu.css'
 import {NavLink} from 'react-router-dom'
 
 const Menu = () => {
+
+	const [toggleMenu, setToggleMenu] = useState (false)
+
+	
+
 	return (
 		<div className="Menu">
 			<ul>
 				<li>
-					<NavLink to='/my-orders' >My Orders</NavLink>
+					<NavLink to='/my-orders' onClick={() => setToggleMenu.remove (toggleMenu)} >My Orders</NavLink>
 				</li>
 				<li>
-					<NavLink to='/my-account'>My account</NavLink>
+					<NavLink to='/my-account' onClick={() => setToggleMenu.remove (toggleMenu)}>My account</NavLink>
 				</li>
 				<li>
-					<NavLink to='/login'>Sign out</NavLink>
+					<NavLink to='/login' onClick={() => setToggleMenu.remove (toggleMenu)}>Sign out</NavLink>
 				</li>
 			</ul>
 		</div>
